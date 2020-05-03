12c36ef5a6a3e1f523bb06455c7ac369e61a6b43
Sam Monk <monk_sam@hotmail.com>
2020-05-03 15:57:12 -0400

    repairing query to address data format change

--- get_usa_data_db_to_json_copy.py
+++ get_usa_data_db_to_json_copy.py
@@ -14,8 +14,9 @@ metadata = db.MetaData()
 db.Table('situation_report_data', metadata, autoload=True, autoload_with=engine)
 table = metadata.tables['situation_report_data']
 conn = engine.connect()
-select_statement = table.select().where(table.c.country == "United States of\rAmerica")
+select_statement = table.select().where(table.c.country.ilike("United States of%America"))
 select_return = conn.execute(select_statement)
 data = json.dumps([dict(r) for r in select_return], default=default)
+print(data)
 with open('./all_usa_data.json', 'w+') as outfile:
     outfile.write(data)
